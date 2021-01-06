84204f6a2277d80ebf314b6cdcc7e508c5870e89
phi38 <pcottaz@yahoo.fr>
2021-01-06 11:58:12 +0100

    apiresource attribut for user

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,17 +2,33 @@
 
 namespace App\Entity;
 
-use App\Repository\UserRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use App\Repository\UserRepository;
+use Doctrine\Common\Collections\Collection;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
  * @ORM\Table(name="`user`")
  * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
+ * @ApiResource(
+ *      collectionOperations={
+ *          "post" ={"path"="/apprenti"},
+ *      }, 
+ *      itemOperations={
+ *          "put" ={
+ *                  "path"="/apprenti/{id}",
+ *                  "normalizationContext"={"groups"={"apprenti:read","apprenti:write"}} 
+ *                  }
+ *      },
+ *      normalizationContext={"groups"={"apprenti:read"}, "swagger_definition_name"="Read"},
+ *      denormalizationContext={"groups"={"apprenti:write"}, "swagger_definition_name"="Write"},
+ *      shortName="Apprenti"
+ * )
  */
 class User implements UserInterface
 {
@@ -25,6 +41,7 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=180, unique=true)
+     * @Groups({"apprenti:write"})
      */
     private $email;
 
@@ -36,11 +53,13 @@ class User implements UserInterface
     /**
      * @var string The hashed password
      * @ORM\Column(type="string")
+     * @Groups({"apprenti:write"})
      */
     private $password;
 
     /**
      * @ORM\Column(type="string", length=30, nullable=true)
+     * @Groups({"apprenti:write","apprenti:read"})
      */
     private $pseudo;
 
