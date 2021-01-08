6ad564193989e488261bb5ddefbdf1bfa50f13ba
phi38 <pcottaz@yahoo.fr>
2021-01-08 14:58:37 +0100

    cursus groups

--- src/Entity/Cursus.php
+++ src/Entity/Cursus.php
@@ -2,27 +2,38 @@
 
 namespace App\Entity;
 
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
+use App\Entity\CursusContent;
 use Doctrine\ORM\Mapping as ORM;
 use App\Repository\CursusRepository;
-use ApiPlatform\Core\Annotation\ApiResource;
 use ApiPlatform\Core\Annotation\ApiFilter;
-use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
+use Doctrine\Common\Collections\Collection;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Serializer\Annotation\Groups;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\DateFilter;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\RangeFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
 
 /**
  * @ORM\Entity(repositoryClass=CursusRepository::class)
  * @ApiResource(
  *  collectionOperations={
- *          "get" ={"path"="/cursus"}
+ *          "get" ={
+ *              "path"="/cursus",
+ *               "normalization_context"={"groups"={"cursussimple:read"}} 
+ *              }
  *      }, 
  *  itemOperations={
- *          "get" ={"path"="/cursus/{id}"}
+ *          "get" ={
+ *              "path"="/cursus/{id}",
+ *              "normalization_context"={"groups"={"cursusdetail:read"}} 
+ *              }
  *      }
  * )
+ * 
  * @ApiFilter(SearchFilter::class,  properties={"level":"exact"})
  * @ApiFilter(RangeFilter::class, properties={"level"})
+ * @ApiFilter(DateFilter::class, properties={"lastupdate":"strictly_after"})
  **/
 class Cursus
 {
@@ -30,51 +41,71 @@ class Cursus
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
+     * 
+     * @Groups({"cursussimple:read"})
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=100)
+     * 
+     * @Groups({"cursussimple:read"})
      */
     private $title;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * 
+     * @Groups({"cursussimple:read"})
      */
     private $description;
 
     /**
      * @ORM\Column(type="integer")
+     * 
+     * @Groups({"cursussimple:read"})
      */
     private $level;
 
     /**
      * @ORM\Column(type="integer")
+     * 
+     * @Groups({"cursussimple:read"})
      */
     private $rights;
 
     /**
      * @ORM\Column(type="integer")
+     * 
+     * @Groups({"cursussimple:read"})
      */
     private $points;
 
     /**
      * @ORM\Column(type="datetime_immutable")
+     * 
+     * @Groups({"cursussimple:read"})
      */
     private $lastupdate;
 
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
+     * 
+     * @Groups({"cursussimple:read"})
      */
     private $theme;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * 
+     * @Groups({"cursussimple:read"})
      */
     private $subtitle;
 
     /**
      * @ORM\OneToMany(targetEntity=CursusContent::class, mappedBy="cursus", orphanRemoval=true)
+     * 
+     * @Groups({"cursusdetail:read"})
      */
     private $cursusContents;
 
@@ -160,16 +191,33 @@ class Cursus
         return $this;
     }
 
+
+    /**
+     * How long ago in text that this cheese listing was added.
+     *
+     * @Groups("cursussimple:read")
+     * 
+     */
     public function getNbComments(): ?int
     {
-        return 0;
+        return 10;
     }
 
+    /**
+     * How long ago in text that this cheese listing was added.
+     *
+     * @Groups("cursussimple:read")
+     */
     public function getNotation(): int
     {
         return 5;
     }
 
+    /**
+     * How long ago in text that this cheese listing was added.
+     *
+     * @Groups("cursussimple:read")
+     */
     public function getNbrecord(): int
     {
         return 5;
